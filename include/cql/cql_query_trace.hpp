ed27da5f3089e107f65097d8a5239fa164101a32
Juliusz Stasiewicz <jstasiewicz@cognitum.eu>
2014-03-29 18:13:23 +0100

    query tracing with new uuids + excluded query tracing impl from header

--- include/cql/cql_query_trace.hpp
+++ include/cql/cql_query_trace.hpp
@@ -3,22 +3,19 @@
 
 #include "cql/cql.hpp"
 #include "cql/cql_map.hpp"
-#include "cql/exceptions/cql_exception.hpp"
+#include "cql/cql_connection.hpp"
+#include "cql/cql_uuid.hpp"
 
-#include <boost/format.hpp>
 #include <boost/noncopyable.hpp>
+#include <boost/asio/ip/address.hpp>
 
 #include <string>
 #include <list>
 
 namespace cql {
 
-struct cql_trace_id_t { // TODO (JS) this should (?) be cql_uuid_t
-    cql_trace_id_t(const std::string& trace_id_string_)
-        : trace_id_string(trace_id_string_) {}
-    
-    std::string trace_id_string;
-};
+class cql_session_t;
+class cql_query_t;
     
 //-------------------------------------------------------------------------------------
     
@@ -26,22 +23,18 @@ struct CQL_EXPORT cql_trace_event_t {
     
     cql_trace_event_t(
         const std::string               name_,
-        cql_bigint_t                    timestamp_,
+        cql::cql_bigint_t               timestamp_,
         const boost::asio::ip::address& source_,
-        cql_bigint_t                    source_elapsed_,
+        cql::cql_bigint_t               source_elapsed_,
         const std::string&              thread_name_)
             : name(name_),
-              timestamp(timestamp_),
-              source(source_),
-              source_elapsed(source_elapsed_),
-              thread_name(thread_name_) {}
-    
+             timestamp(timestamp_),
+             source(source_),
+             source_elapsed(source_elapsed_),
+             thread_name(thread_name_) {}
+
     std::string
-    to_string() {
-        return boost::str(
-                boost::format("%1% on %2%[%3%] at %4%")
-                    % name % source % thread_name % timestamp);
-    }
+    to_string() const;
     
     std::string              name;
     cql_bigint_t             timestamp;
@@ -57,7 +50,7 @@ class CQL_EXPORT cql_query_trace_t :
 {
 public:
     cql_query_trace_t(
-        const cql_trace_id_t&                trace_id,
+        const cql_uuid_t&                    trace_id,
         boost::shared_ptr<cql_session_t>     session,
         cql_connection_t::cql_log_callback_t log_callback = 0)
             : _trace_id(trace_id),
@@ -73,27 +66,27 @@ public:
         }
     }
     
-    cql_trace_id_t
+    cql_uuid_t
     get_trace_id() const {
         return _trace_id;
     }
 
     /** If the result is unavailable yet, the function returns empty string. */
-    std::string
+    inline std::string
     get_request_type() {
         maybe_fetch_trace();
         return _request_type;
     }
     
     /** If the result is unavailable yet, the function returns cql_query_trace_t::UNAVAILABLE_YET. */
-    cql_bigint_t
+    inline cql_bigint_t
     get_duration_micros() {
         maybe_fetch_trace();
         return _duration;
     }
     
     /** If the result is unavailable yet, the function returns empty IP address. */
-    boost::asio::ip::address
+    inline boost::asio::ip::address
     get_coordinator() {
         maybe_fetch_trace();
         return _coordinator;
@@ -103,7 +96,7 @@ public:
         Otherwise, the argument pointer points to the map of parameters.
         Caller (you) DOES NOT have the ownership of this pointer! 
         Lifetime of the underlying map equals the lifetime of this cql_query_trace_t object. */
-    bool
+    inline bool
     get_parameters(cql_map_t** parameters_) { // TODO(JS): some smart ptr here?
         if (maybe_fetch_trace()) {
             *parameters_ = _parameters;
@@ -113,7 +106,7 @@ public:
     }
     
     /** If the result is unavailable yet, the function returns zero. */
-    cql_bigint_t
+    inline cql_bigint_t
     started_at() {
         maybe_fetch_trace();
         return _started_at;
@@ -122,7 +115,7 @@ public:
     /** Returns false if the result is unavailable yet.
         Otherwise, the list of events is written under the reference argument.
         Any content of the list gets disregarded. */
-    bool
+    inline bool
     get_events(std::list<cql_trace_event_t>& events_) {
         if (maybe_fetch_trace()) {
             events_ = _events; // Copy the contents of a list.
@@ -132,161 +125,29 @@ public:
     }
     
     std::string
-    to_string() const {
-        return boost::str(
-                boost::format("%1% [%2%] - %3%us") % _request_type % _trace_id.trace_id_string % _duration);
-    }
+    to_string() const;
     
 private:
     
     /** Returns true if trace is available; false otherwise. */
     bool
-    maybe_fetch_trace() {
-        if (_duration != UNAVAILABLE_YET) {
-            return true;
-        }
-        boost::mutex::scoped_lock lock(_mutex);
-        if (_duration == UNAVAILABLE_YET) {
-            return do_fetch_trace();
-        }
-        else return true;
-    }
+    maybe_fetch_trace();
     
     /** Returns true if trace is available; false otherwise. */
     bool
-    do_fetch_trace() {
-        try {
-            {// Scope limiter
-                boost::shared_future<cql_future_result_t> future_result
-                    = _session->query(select_sessions_query());
-            
-                if (!(future_result.timed_wait(boost::posix_time::seconds(30)))) {
-                    if (_log_callback) {
-                        _log_callback(CQL_LOG_ERROR,
-                                      "Query against system_traces.sessions timed out");
-                    }
-                    return false;
-                }
-                else {
-                    cql_future_result_t query_result = future_result.get();
-                
-                    if (query_result.error.is_err()) {
-                        if (_log_callback) {
-                            _log_callback(CQL_LOG_ERROR,
-                                          "Error while querying system_traces.sessions: " + query_result.error.message);
-                        }
-                        return false;
-                    }
-                
-                    boost::shared_ptr<cql::cql_result_t> result = query_result.result;
-                    while (result->next()) {
-                        bool is_empty = true;
-                        if (!result->get_nullity("request", is_empty) && !is_empty) {
-                            result->get_string("request", _request_type);
-                        }
-                        if (!result->get_nullity("duration", is_empty) && !is_empty) {
-                            result->get_bigint("duration", _duration);
-                        }
-                        if (!result->get_nullity("coordinator", is_empty) && !is_empty) {
-                        // TODO(JS): deserialize ip addresses at last.
-                        //    result->get_ip_address("coordinator", _coordinator);
-                        }
-                        if (!result->get_nullity("parameters", is_empty) && !is_empty) {
-                            result->get_map("parameters", &_parameters);
-                        }
-                        if (!result->get_nullity("started_at", is_empty) && !is_empty) {
-                            result->get_bigint("started_at", _started_at);
-                        }
-                        break;
-                    }
-                }
-            }// End of scope limiter
-            {// Scope limiter
-                _events.clear();
-                
-                boost::shared_future<cql_future_result_t> future_result
-                    = _session->query(select_events_query());
-            
-                if (!(future_result.timed_wait(boost::posix_time::seconds(30)))) {
-                    if (_log_callback) {
-                        _log_callback(CQL_LOG_ERROR,
-                                      "Query against system_traces.events timed out");
-                    }
-                    return false;
-                }
-                else {
-                    cql_future_result_t query_result = future_result.get();
-                
-                    if (query_result.error.is_err()) {
-                        if (_log_callback) {
-                            _log_callback(CQL_LOG_ERROR,
-                                          "Error while querying system_traces.events: " + query_result.error.message);
-                        }
-                        return false;
-                    }
-                
-                    boost::shared_ptr<cql::cql_result_t> result = query_result.result;
-                    while (result->next()) {
-                        std::string activity, thread, event_id;
-                        cql_bigint_t source_elapsed = 0;
-                        boost::asio::ip::address source;
-                        
-                        bool is_empty = true;
-                        
-                        if (!result->get_nullity("activity", is_empty) && !is_empty) {
-                            result->get_string("activity", activity);
-                        }
-                        if (!result->get_nullity("event_id", is_empty) && !is_empty) {
-                            result->get_string("event_id", event_id); // TODO (JS) this should (?) be cql_uuid_t
-                        }
-                        if (!result->get_nullity("source", is_empty) && !is_empty) {
-                            // TODO(JS) :
-                            // result->get_ip_address("source", source);
-                        }
-                        if (!result->get_nullity("source_elapsed", is_empty) && !is_empty) {
-                            result->get_bigint("source_elapsed", source_elapsed);
-                        }
-                        if (!result->get_nullity("thread", is_empty) && !is_empty) {
-                            result->get_string("thread", thread);
-                        }
-                        
-                        cql_bigint_t timestamp = 0; // TODO (JS) this should (?) be cql_uuid_t
-                        cql_trace_event_t event(activity, timestamp, source, source_elapsed, thread);
-                        _events.push_back(event);
-                    }
-                }
-            }// End of scope limiter
-        }
-        catch (cql_exception& e) {
-            if (_log_callback) {
-                _log_callback(CQL_LOG_ERROR,
-                              std::string("Unexpected exception while fetching query trace ") + e.what());
-            }
-        }
-        return true;
-    }
+    do_fetch_trace();
     
     boost::shared_ptr<cql_query_t>
-    select_sessions_query() const {
-        std::string query_text = boost::str(boost::format(
-                                    "SELECT * FROM system_traces.sessions WHERE session_id = %s")
-                                    % _trace_id.trace_id_string);
-        return boost::shared_ptr<cql_query_t>(new cql_query_t(query_text));
-    }
+    select_sessions_query() const;
     
     boost::shared_ptr<cql_query_t>
-    select_events_query() const {
-        std::string query_text = boost::str(boost::format(
-                                    "SELECT * FROM system_traces.events WHERE session_id = %s")
-                                    % _trace_id.trace_id_string);
-        return boost::shared_ptr<cql_query_t>(new cql_query_t(query_text));
-    }
+    select_events_query() const;
     
     static const cql_bigint_t            UNAVAILABLE_YET = -1;
     
     boost::mutex                         _mutex;
     
-    cql_trace_id_t                       _trace_id;
+    cql_uuid_t                           _trace_id;
     boost::shared_ptr<cql_session_t>     _session;
     cql_connection_t::cql_log_callback_t _log_callback;
     
---
+++ src/cql/cql_query_trace.cpp
@@ -0,0 +1,162 @@
+#include "cql/cql_query_trace.hpp"
+#include "cql/cql_query.hpp"
+#include "cql/cql_session.hpp"
+#include "cql/exceptions/cql_exception.hpp"
+
+#include <boost/format.hpp>
+
+//-------------------------------------------------------------------------------------
+std::string
+cql::cql_trace_event_t::to_string() const {
+    return boost::str(
+            boost::format("%1% on %2%[%3%] at %4%")
+                % name % source % thread_name % timestamp);
+}
+//------------------------------------------------------------------------------------
+std::string
+cql::cql_query_trace_t::to_string() const {
+    return boost::str(
+            boost::format("%1% [%2%] - %3%us")
+                % _request_type % _trace_id.to_string() % _duration);
+}
+    
+bool
+cql::cql_query_trace_t::maybe_fetch_trace() {
+    if (_duration != UNAVAILABLE_YET) {
+        return true;
+    }
+    boost::mutex::scoped_lock lock(_mutex);
+    if (_duration == UNAVAILABLE_YET) {
+        return do_fetch_trace();
+    }
+    else return true;
+}
+    
+bool
+cql::cql_query_trace_t::do_fetch_trace() {
+    try {
+        {// Scope limiter
+            boost::shared_future<cql_future_result_t> future_result
+                = _session->query(select_sessions_query());
+        
+            if (!(future_result.timed_wait(boost::posix_time::seconds(30)))) {
+                if (_log_callback) {
+                    _log_callback(CQL_LOG_ERROR,
+                                    "Query against system_traces.sessions timed out");
+                }
+                return false;
+            }
+            else {
+                cql_future_result_t query_result = future_result.get();
+                
+                if (query_result.error.is_err()) {
+                    if (_log_callback) {
+                        _log_callback(CQL_LOG_ERROR,
+                                        "Error while querying system_traces.sessions: " + query_result.error.message);
+                    }
+                    return false;
+                }
+                
+                boost::shared_ptr<cql::cql_result_t> result = query_result.result;
+                while (result->next()) {
+                    bool is_empty = true;
+                    if (!result->get_nullity("request", is_empty) && !is_empty) {
+                        result->get_string("request", _request_type);
+                    }
+                    if (!result->get_nullity("duration", is_empty) && !is_empty) {
+                        result->get_bigint("duration", _duration);
+                    }
+                    if (!result->get_nullity("coordinator", is_empty) && !is_empty) {
+                    // TODO(JS): deserialize ip addresses at last.
+                    //    result->get_ip_address("coordinator", _coordinator);
+                    }
+                    if (!result->get_nullity("parameters", is_empty) && !is_empty) {
+                        result->get_map("parameters", &_parameters);
+                    }
+                    if (!result->get_nullity("started_at", is_empty) && !is_empty) {
+                        result->get_bigint("started_at", _started_at);
+                    }
+                    break;
+                }
+            }
+        }// End of scope limiter
+        {// Scope limiter
+            _events.clear();
+            
+            boost::shared_future<cql_future_result_t> future_result
+                = _session->query(select_events_query());
+            
+            if (!(future_result.timed_wait(boost::posix_time::seconds(30)))) {
+                if (_log_callback) {
+                    _log_callback(CQL_LOG_ERROR,
+                                    "Query against system_traces.events timed out");
+                }
+                return false;
+            }
+            else {
+                cql_future_result_t query_result = future_result.get();
+            
+                if (query_result.error.is_err()) {
+                    if (_log_callback) {
+                        _log_callback(CQL_LOG_ERROR,
+                                        "Error while querying system_traces.events: " + query_result.error.message);
+                    }
+                    return false;
+                }
+                
+                boost::shared_ptr<cql::cql_result_t> result = query_result.result;
+                while (result->next()) {
+                    std::string activity, thread, event_id;
+                    cql_bigint_t source_elapsed = 0;
+                    boost::asio::ip::address source;
+                    
+                    bool is_empty = true;
+                        
+                    if (!result->get_nullity("activity", is_empty) && !is_empty) {
+                        result->get_string("activity", activity);
+                    }
+                    if (!result->get_nullity("event_id", is_empty) && !is_empty) {
+                        result->get_string("event_id", event_id); // TODO (JS) this should (?) be cql_uuid_t
+                    }
+                    if (!result->get_nullity("source", is_empty) && !is_empty) {
+                        // TODO(JS) :
+                        // result->get_ip_address("source", source);
+                    }
+                    if (!result->get_nullity("source_elapsed", is_empty) && !is_empty) {
+                        result->get_bigint("source_elapsed", source_elapsed);
+                    }
+                    if (!result->get_nullity("thread", is_empty) && !is_empty) {
+                        result->get_string("thread", thread);
+                    }
+                    
+                    cql_bigint_t timestamp = 0; // TODO (JS) this should (?) be cql_uuid_t
+                    cql_trace_event_t event(activity, timestamp, source, source_elapsed, thread);
+                    _events.push_back(event);
+                }
+            }
+        }// End of scope limiter
+    }
+    catch (cql_exception& e) {
+        if (_log_callback) {
+            _log_callback(CQL_LOG_ERROR,
+                            std::string("Unexpected exception while fetching query trace ") + e.what());
+        }
+    }
+    return true;
+}
+    
+boost::shared_ptr<cql::cql_query_t>
+cql::cql_query_trace_t::select_sessions_query() const {
+    std::string query_text = boost::str(boost::format(
+                                "SELECT * FROM system_traces.sessions WHERE session_id = %s")
+                                % _trace_id.to_string());
+    return boost::shared_ptr<cql_query_t>(new cql_query_t(query_text));
+}
+    
+boost::shared_ptr<cql::cql_query_t>
+cql::cql_query_trace_t::select_events_query() const {
+    std::string query_text = boost::str(boost::format(
+                                "SELECT * FROM system_traces.events WHERE session_id = %s")
+                                % _trace_id.to_string());
+    return boost::shared_ptr<cql_query_t>(new cql_query_t(query_text));
+}
