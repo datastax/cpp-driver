6d074bba1dbdd1d687c1be7154b026e863bee0de
bbucher <bbucher@datastax.com>
2014-06-05 14:19:03 -0500

    adding real prepared statement test

--- test/integration_tests/src/batch.cpp
+++ test/integration_tests/src/batch.cpp
@@ -30,10 +30,46 @@ BOOST_FIXTURE_TEST_SUITE(batch, BatchTests)
 
 BOOST_AUTO_TEST_CASE(test_prepared)
 {
-  CassFuture* temp_future;
-  test_utils::CassSessionPtr session(cass_cluster_connect(cluster, &temp_future));
-  test_utils::CassFuturePtr session_future(temp_future);
+    test_utils::CassFuturePtr session_future(cass_cluster_connect(cluster));
+    test_utils::wait_and_check_error(session_future.get());
+    test_utils::CassSessionPtr temp_session(cass_future_get_session(session_future.get()));
+
+    CassBatch* batch = cass_batch_new(CASS_CONSISTENCY_ONE);
+    std::string insert_query = str(boost::format("INSERT INTO %s (tweet_id, test_val) VALUES(?, ?);") % BatchTests::SIMPLE_TABLE_NAME);
+    test_utils::CassPreparedPtr prepared_statement;
+    test_utils::CassStatementPtr insert_statement(cass_prepared_bind(prepared_statement.get(), 2, CASS_CONSISTENCY_ONE));
+
+    for (int x=0; x<4; x++)
+    {
+        BOOST_REQUIRE(cass_statement_bind_int32(insert_statement.get(), 0, x) == CASS_OK);
+        BOOST_REQUIRE(cass_statement_bind_string(insert_statement.get(), 1, cass_string_init(str(boost::format("test data %s") % x).c_str())) == CASS_OK);
+        cass_batch_add_statement(batch, insert_statement.get());
+    }
+    test_utils::CassFuturePtr insert_future(cass_session_execute_batch(temp_session.get(), batch));
+    test_utils::wait_and_check_error(insert_future.get());
+
+    for (int y=0; y<4; y++)
+    {
+      std::string select_query = str(boost::format("SELECT * FROM %s WHERE tweet_id = ?;") % BatchTests::SIMPLE_TABLE_NAME);
+      test_utils::CassStatementPtr select_statement(cass_statement_new(cass_string_init(select_query.c_str()), 1, CASS_CONSISTENCY_ONE));
+      BOOST_REQUIRE(cass_statement_bind_int32(select_statement.get(), 0, y) == CASS_OK);
+      test_utils::CassFuturePtr select_future(cass_session_execute(temp_session.get(), select_statement.get()));
+      test_utils::wait_and_check_error(select_future.get());
+      test_utils::CassResultPtr result(cass_future_get_result(select_future.get()));
+      const CassValue* column = cass_row_get_column(cass_result_first_row(result.get()), 1);
+
+      CassString result_value;
+      BOOST_REQUIRE(cass_value_type(column) == CASS_VALUE_TYPE_TEXT);
+      BOOST_REQUIRE(test_utils::Value<CassString>::get(column, &result_value) == CASS_OK);
+      BOOST_REQUIRE(test_utils::Value<CassString>::equal(result_value, cass_string_init(str(boost::format("test data %s") % y).c_str())));
+    }
+}
+
+BOOST_AUTO_TEST_CASE(test_simple)
+{
+  test_utils::CassFuturePtr session_future(cass_cluster_connect(cluster));
   test_utils::wait_and_check_error(session_future.get());
+  test_utils::CassSessionPtr temp_session(cass_future_get_session(session_future.get()));
 
   CassBatch* batch = cass_batch_new(CASS_CONSISTENCY_ONE);
   std::string insert_query = str(boost::format("INSERT INTO %s (tweet_id, test_val) VALUES(?, ?);") % BatchTests::SIMPLE_TABLE_NAME);
@@ -46,7 +82,7 @@ BOOST_AUTO_TEST_CASE(test_prepared)
     cass_batch_add_statement(batch, insert_statement.get());
   }
 
-  test_utils::CassFuturePtr insert_future(cass_session_execute_batch(session.get(), batch));
+  test_utils::CassFuturePtr insert_future(cass_session_execute_batch(temp_session.get(), batch));
   test_utils::wait_and_check_error(insert_future.get());
 
   for (int y=0; y<4; y++)
@@ -54,7 +90,7 @@ BOOST_AUTO_TEST_CASE(test_prepared)
     std::string select_query = str(boost::format("SELECT * FROM %s WHERE tweet_id = ?;") % BatchTests::SIMPLE_TABLE_NAME);
     test_utils::CassStatementPtr select_statement(cass_statement_new(cass_string_init(select_query.c_str()), 1, CASS_CONSISTENCY_ONE));
     BOOST_REQUIRE(cass_statement_bind_int32(select_statement.get(), 0, y) == CASS_OK);
-    test_utils::CassFuturePtr select_future(cass_session_execute(session.get(), select_statement.get()));
+    test_utils::CassFuturePtr select_future(cass_session_execute(temp_session.get(), select_statement.get()));
     test_utils::wait_and_check_error(select_future.get());
     test_utils::CassResultPtr result(cass_future_get_result(select_future.get()));
     const CassValue* column = cass_row_get_column(cass_result_first_row(result.get()), 1);
