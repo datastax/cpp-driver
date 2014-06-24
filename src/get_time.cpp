cdb0b616e8e0d83dafdb3b89820e532ee588723d
mpenick <michael.penick@datastax.com>
2014-06-24 13:01:22 -0700

    Simpler version of get_time_since_epoch() for mac

--- src/get_time.cpp
+++ src/get_time.cpp
@@ -3,8 +3,7 @@
 #if defined(WIN32) || defined(_WIN32)
 #include <Windows.h>
 #elif defined(__APPLE__) && defined(__MACH__)
-#include <mach/mach.h>
-#include <mach/clock.h>
+#include <sys/time.h>
 #else
 #include <time.h>
 #endif
@@ -24,12 +23,9 @@ uint64_t get_time_since_epoch() {
 #elif defined(__APPLE__) && defined(__MACH__)
 
 uint64_t get_time_since_epoch() {
-  clock_serv_t clock_serv;
-  mach_timespec_t ts;
-  host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &clock_serv);
-  clock_get_time(clock_serv, &ts);
-  mach_port_deallocate(mach_task_self(), clock_serv);
-  return ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec * 1000 + tv.tv_usec / 1000;
 }
 
 #else
