87e5155019797408f27ac8e2ad54b6ba281308eb
mpenick <michael.penick@datastax.com>
2014-08-12 15:24:34 -0700

    Fixed unit test build and added invalid port test

--- test/integration_tests/src/sessions.cpp
+++ test/integration_tests/src/sessions.cpp
@@ -79,6 +79,31 @@ BOOST_AUTO_TEST_CASE(test_connect_invalid_name)
   BOOST_CHECK_EQUAL(code, CASS_ERROR_LIB_NO_HOSTS_AVAILABLE);
 }
 
+BOOST_AUTO_TEST_CASE(test_connect_invalid_port)
+{
+  test_utils::CassClusterPtr cluster(cass_cluster_new());
+
+  const cql::cql_ccm_bridge_configuration_t& conf = cql::get_ccm_bridge_configuration();
+  boost::shared_ptr<cql::cql_ccm_bridge_t> ccm = cql::cql_ccm_bridge_t::create(conf, "test", 1, 0);
+
+  test_utils::initialize_contact_points(cluster.get(), conf.ip_prefix(), 1, 0);
+
+  cass_cluster_set_port(cluster.get(), 9999); // Invalid port
+
+  test_utils::CassFuturePtr session_future(cass_cluster_connect(cluster.get()));
+  test_utils::wait_and_check_error(session_future.get());
+  test_utils::CassSessionPtr session(cass_future_get_session(session_future.get()));
+
+  CassString query = cass_string_init("SELECT * FROM table");
+  test_utils::CassStatementPtr statement(cass_statement_new(query, 0));
+
+  test_utils::CassFuturePtr future(cass_session_execute(session.get(), statement.get()));
+
+  CassError code = cass_future_error_code(future.get());
+
+  BOOST_CHECK_EQUAL(code, CASS_ERROR_LIB_NO_HOSTS_AVAILABLE);
+}
+
 BOOST_AUTO_TEST_CASE(test_connect_invalid_keyspace)
 {
   boost::scoped_ptr<test_utils::LogData> log_data(new test_utils::LogData("'Error response during startup: 'Keyspace 'invalid' does not exist' (0x02002200)' error on startup"));
---
+++ test/unit_tests/src/stream_manager.cpp
@@ -0,0 +1,17 @@
+#define BOOST_TEST_DYN_LINK
+#ifdef STAND_ALONE
+#   define BOOST_TEST_MODULE cassandra
+#endif
+
+#include "stream_manager.hpp"
+
+#include <boost/test/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_SUITE(stream_manager)
+
+BOOST_AUTO_TEST_CASE(test_stream_manager)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
