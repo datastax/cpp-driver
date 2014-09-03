60745b0d1651990951f818213ca51aa776f46742
mpenick <michael.penick@datastax.com>
2014-09-03 15:48:56 -0700

    Added integration tests for node discovery (CPP-145)

--- test/integration_tests/src/control_connection.cpp
+++ test/integration_tests/src/control_connection.cpp
@@ -19,9 +19,12 @@
 #   define BOOST_TEST_MODULE cassandra
 #endif
 
+#include "address.hpp"
 #include "cassandra.h"
-#include "test_utils.hpp"
+#include "types.hpp"
+
 #include "cql_ccm_bridge.hpp"
+#include "test_utils.hpp"
 
 #include <boost/test/unit_test.hpp>
 
@@ -61,6 +64,74 @@ BOOST_AUTO_TEST_CASE(test_connect_invalid_port)
   CassError code = test_utils::wait_and_return_error(session_future.get());
 
   BOOST_CHECK_EQUAL(code, CASS_ERROR_LIB_NO_HOSTS_AVAILABLE);
+}
+
+BOOST_AUTO_TEST_CASE(test_node_discovery)
+{
+  test_utils::CassClusterPtr cluster(cass_cluster_new());
+
+  const cql::cql_ccm_bridge_configuration_t& conf = cql::get_ccm_bridge_configuration();
+  boost::shared_ptr<cql::cql_ccm_bridge_t> ccm = cql::cql_ccm_bridge_t::create(conf, "test", 3, 0);
+
+  // Ensure RR policy
+  cass_cluster_set_load_balance_round_robin(cluster.get());;
+
+  // Only add a single IP
+  test_utils::initialize_contact_points(cluster.get(), conf.ip_prefix(), 1, 0);
+
+  test_utils::CassFuturePtr session_future(cass_cluster_connect(cluster.get()));
+  test_utils::wait_and_check_error(session_future.get());
+  test_utils::CassSessionPtr session(cass_future_get_session(session_future.get()));
+
+  cass::AddressSet addresses;
+  for (int i = 0; i < 3; ++i) {
+    CassString query = cass_string_init("SELECT * FROM system.schema_keyspaces");
+    test_utils::CassStatementPtr statement(cass_statement_new(query, 0));
+    test_utils::CassFuturePtr future(cass_session_execute(session.get(), statement.get()));
+    addresses.insert(static_cast<cass::ResponseFuture*>(future->from())->get_host_address());
+  }
+
+  BOOST_CHECK(addresses.size() == 3);
+}
+
+BOOST_AUTO_TEST_CASE(test_node_discovery_invalid_ips)
+{
+  boost::scoped_ptr<test_utils::LogData> log_data(new test_utils::LogData("Unable to reach contact point 192.0.2."));
+
+  {
+    test_utils::CassClusterPtr cluster(cass_cluster_new());
+
+
+    const cql::cql_ccm_bridge_configuration_t& conf = cql::get_ccm_bridge_configuration();
+    boost::shared_ptr<cql::cql_ccm_bridge_t> ccm = cql::cql_ccm_bridge_t::create(conf, "test", 3, 0);
+
+    cass_cluster_set_log_callback(cluster.get(), test_utils::count_message_log_callback, log_data.get());
+
+    // Ensure RR policy
+    cass_cluster_set_load_balance_round_robin(cluster.get());;
+
+    // Add invalid IPs first (http://tools.ietf.org/html/rfc5737)
+    cass_cluster_set_contact_points(cluster.get(), "192.0.2.0,192.0.2.1,192.0.2.3");
+
+    // Only add a single valid IP
+    test_utils::initialize_contact_points(cluster.get(), conf.ip_prefix(), 1, 0);
+
+    test_utils::CassFuturePtr session_future(cass_cluster_connect(cluster.get()));
+    test_utils::wait_and_check_error(session_future.get());
+    test_utils::CassSessionPtr session(cass_future_get_session(session_future.get()));
+
+    cass::AddressSet addresses;
+    for (int i = 0; i < 4; ++i) {
+      CassString query = cass_string_init("SELECT * FROM system.schema_keyspaces");
+      test_utils::CassStatementPtr statement(cass_statement_new(query, 0));
+      test_utils::CassFuturePtr future(cass_session_execute(session.get(), statement.get()));
+      addresses.insert(static_cast<cass::ResponseFuture*>(future->from())->get_host_address());
+    }
+
+    BOOST_CHECK(addresses.size() == 3);
+  }
+
+  BOOST_CHECK_EQUAL(log_data->message_count, 3);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
