b80575a3d21af87a26c3ecd496a0d8c300780423
Adam Holmberg <Adam.Holmberg@datastax.com>
2014-09-03 08:49:07 -0500

    Accept authenticate message in any protocol version.

--- src/auth_responses.cpp
+++ src/auth_responses.cpp
@@ -21,9 +21,6 @@
 namespace cass {
 
 bool AuthenticateResponse::decode(int version, char* buffer, size_t size) {
-  if (version != 1) {
-    return false;
-  }
   decode_string(buffer, &authenticator_, authenticator_size_);
   return true;
 }
