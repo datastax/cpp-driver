8ed5881d8e3b6411e378f96631e8e6d66dd8bb75
Michael Fero <michael.fero@datastax.com>
2015-04-13 14:49:40 -0700

    test: Code review updates

--- test/integration_tests/src/test_latency_aware_policy.cpp
+++ test/integration_tests/src/test_latency_aware_policy.cpp
@@ -29,6 +29,7 @@
 #include "test_utils.hpp"
 
 #define DEFAULT_CASSANDRA_NODE_PORT 9042
+#define WARM_UP_QUERY_COUNT 25
 
 struct LatencyAwarePolicyTest {
 public:
@@ -37,14 +38,13 @@ public:
   LatencyAwarePolicyTest()
     : configuration_(cql::get_ccm_bridge_configuration())
     , cluster_(cass_cluster_new())
-    , thread_()
-#if UV_VERSION_MAJOR == 0
-    , loop_(uv_loop_new()) {
-# else
-  {
+    , thread_() {
     boost::debug::detect_memory_leaks(false);
-    uv_loop_init(&loop_);
-#endif
+    boost::unit_test::unit_test_log_t::instance().set_threshold_level(boost::unit_test::log_messages);
+
+    uv_mutex_init(&lock_);
+    uv_cond_init(&condition_); 
+
     // Initialize the cluster for latency aware
     cass_cluster_set_reconnect_wait_time(cluster_.get(), 1);
     cass_cluster_set_connect_timeout(cluster_.get(), 240 * test_utils::ONE_SECOND_IN_MICROS);
@@ -61,12 +61,8 @@ public:
 
   ~LatencyAwarePolicyTest() {
     stop_query_execution();
-#if UV_VERSION_MAJOR == 0
-    uv_loop_delete(loop_);
-    loop - NULL;
-#else
-    uv_loop_close(&loop_);
-#endif
+    uv_mutex_destroy(&lock_);
+    uv_cond_destroy(&condition_);
 
     test_utils::CassFuturePtr close_future(cass_session_close(session_.get()));
     cass_future_wait(close_future.get());
@@ -76,7 +72,18 @@ public:
    * Start the query execution thread
    */
   void start_query_execution() {
+    is_running_ = true;
+    is_error_ = false;
+    is_warming_up_ = true;
+    memset(max_node_latency, 0, sizeof(max_node_latency));
     uv_thread_create(&thread_, start_thread, NULL);
+
+    // Allow metrics to gather some initial data
+    uv_mutex_lock(&lock_);
+    while (is_warming_up_) {
+      uv_cond_wait(&condition_, &lock_);
+    }
+    uv_mutex_unlock(&lock_);
   }
 
   /**
@@ -85,6 +92,9 @@ public:
   void stop_query_execution() {
     is_running_ = false;
     uv_thread_join(&thread_);
+    if (is_error_) {
+      BOOST_FAIL("Error occurred during query execution");
+    }
   }
 
   /**
@@ -112,6 +122,9 @@ public:
       double latency = max_node_latency[node - 1];
       if (latency < expected_latency) {
         BOOST_CHECK_CLOSE(latency, expected_latency, tolerance);
+      } else {
+        // Ensure assertions are checked for testing framework
+        BOOST_CHECK(latency >= expected_latency);
       }
   }
 
@@ -119,13 +132,12 @@ private:
   static test_utils::CassSessionPtr session_;
   const cql::cql_ccm_bridge_configuration_t& configuration_;
   test_utils::CassClusterPtr cluster_;
+  static bool is_error_;
   static bool is_running_;
-#if UV_VERSION_MAJOR == 0
-  static uv_loop_t* loop_;
-#else
-  static uv_loop_t loop_;
-#endif
+  static bool is_warming_up_;
   uv_thread_t thread_;
+  static uv_cond_t condition_;
+  static uv_mutex_t lock_;
   static double max_node_latency[3];
 
   /**
@@ -135,19 +147,16 @@ private:
    */
   static void start_thread(void* arg) {
     execute_query();
-#if UV_VERSION_MAJOR == 0
-    uv_run(loop_, UV_RUN_DEFAULT)
-#else
-    uv_run(&loop_, UV_RUN_DEFAULT);
-#endif
   }
 
   /**
    * Execute a query on the system table
    */
   static void execute_query() {
-    is_running_ = true;
-    while(is_running_) {
+    is_warming_up_ = true;
+    int number_of_queries_executed = 0;
+
+    do {
       std::string query = "SELECT * FROM system.local";
       test_utils::CassStatementPtr statement(cass_statement_new_n(query.data(), query.size(), 0));
       test_utils::CassFuturePtr future(cass_session_execute(session_.get(), statement.get()));
@@ -160,36 +169,56 @@ private:
           error_code != CASS_ERROR_SERVER_READ_TIMEOUT) {
         CassString message;
         cass_future_error_message(future.get(), &message.data, &message.length);
-        BOOST_FAIL(std::string(message.data, message.length) << "' (" << cass_error_desc(error_code) << ")");
+
+        // Indicate error occurred
+        BOOST_TEST_MESSAGE(std::string(message.data, message.length) << "' (" << cass_error_desc(error_code) << ")");
+        is_error_ = true;
+        is_running_ = false;
       }
 
-      // Get the host latency
-      std::string host_ip_address = cass::get_host_from_future(future.get());
-      double host_latency = static_cast<double>(cass::get_host_latency_average(session_.get(),
-                                                                               host_ip_address,
-                                                                               DEFAULT_CASSANDRA_NODE_PORT)) / 1e6;
+      if (!is_error_) {
+        // Get the host latency
+        std::string host_ip_address = cass::get_host_from_future(future.get());
+        double host_latency = static_cast<double>(cass::get_host_latency_average(session_.get(),
+                                                                                 host_ip_address,
+                                                                                 DEFAULT_CASSANDRA_NODE_PORT)) / 1e6;
 
-      // Update the max latency incurred
-      std::stringstream node_value;
-      node_value << host_ip_address[host_ip_address.length() - 1];
-      int node = -1;
-      if (!(node_value >> node).fail()) {
-        if (max_node_latency[node - 1] < host_latency) {
-          max_node_latency[node - 1] = host_latency;
+        // Update the max latency incurred
+        std::stringstream node_value;
+        node_value << host_ip_address[host_ip_address.length() - 1];
+        int node = -1;
+        if (!(node_value >> node).fail()) {
+          if (max_node_latency[node - 1] < host_latency) {
+            max_node_latency[node - 1] = host_latency;
+          }
         }
+
+
+        // Determine if warm up is complete
+        uv_mutex_lock(&lock_);
+        if (++number_of_queries_executed == WARM_UP_QUERY_COUNT) {
+          uv_cond_signal(&condition_);
+          is_warming_up_ = false;
+        }
+        uv_mutex_unlock(&lock_);
       }
+    } while (is_running_);
+
+    // Handle the case where the loop stops before the warm up is complete
+    if (is_warming_up_) {
+      uv_cond_signal(&condition_);
+      is_warming_up_ = false;
     }
   }
 };
 
 // Define static instances
 test_utils::CassSessionPtr LatencyAwarePolicyTest::session_;
+bool LatencyAwarePolicyTest::is_error_ = false;
 bool LatencyAwarePolicyTest::is_running_ = true;
-#if UV_VERSION_MAJOR == 0
-  uv_loop_t* LatencyAwarePolicyTest::loop_ = NULL;
-#else
-  uv_loop_t LatencyAwarePolicyTest::loop_;
-#endif
+bool LatencyAwarePolicyTest::is_warming_up_ = true;
+uv_cond_t LatencyAwarePolicyTest::condition_;
+uv_mutex_t LatencyAwarePolicyTest::lock_;
 double LatencyAwarePolicyTest::max_node_latency[3] = { 0.0 };
 
 BOOST_FIXTURE_TEST_SUITE(latency_aware_policy, LatencyAwarePolicyTest)
@@ -205,40 +234,44 @@ BOOST_FIXTURE_TEST_SUITE(latency_aware_policy, LatencyAwarePolicyTest)
  * @test_category load_balancing_policy:latency_aware
  */
 BOOST_AUTO_TEST_CASE(ensure_latency) {
-  // Start gathering latencies for nodes
-  start_query_execution();
-  boost::this_thread::sleep_for(boost::chrono::milliseconds(1000));
-
   /*
    * Create varying amounts of latency and ensure maximum latency using a threshold
    */
-  create_latency(1, 500);
-  create_latency(2, 500);
-  create_latency(3, 500);
-  check_max_latency(1, 500, 0.05);
-  check_max_latency(2, 500, 0.05);
-  check_max_latency(3, 500, 0.05);
-
-  create_latency(1, 1000);
-  create_latency(2, 1000);
-  create_latency(3, 1000);
-  check_max_latency(1, 1000, 0.025);
-  check_max_latency(2, 1000, 0.025);
-  check_max_latency(3, 1000, 0.025);
-
+  start_query_execution();
   create_latency(1, 2000);
   create_latency(2, 2000);
   create_latency(3, 2000);
+  stop_query_execution();
   check_max_latency(1, 2000, 0.01);
   check_max_latency(2, 2000, 0.01);
   check_max_latency(3, 2000, 0.01);
 
+  start_query_execution();
+  create_latency(1, 500);
+  create_latency(2, 500);
+  create_latency(3, 500);
+  stop_query_execution();
+  check_max_latency(1, 500, 0.05);
+  check_max_latency(2, 500, 0.05);
+  check_max_latency(3, 500, 0.05);
+
+  start_query_execution();
   create_latency(1, 3000);
   create_latency(2, 3000);
   create_latency(3, 3000);
+  stop_query_execution();
   check_max_latency(1, 3000, 0.01);
   check_max_latency(2, 3000, 0.01);
   check_max_latency(3, 3000, 0.01);
+
+  start_query_execution();
+  create_latency(1, 1000);
+  create_latency(2, 1000);
+  create_latency(3, 1000);
+  stop_query_execution();
+  check_max_latency(1, 1000, 0.025);
+  check_max_latency(2, 1000, 0.025);
+  check_max_latency(3, 1000, 0.025);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
