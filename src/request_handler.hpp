0ef6522f95fba486bd2c65119d6f74385ed266e6
Michael Penick <michael.penick@datastax.com>
2015-07-29 15:50:37 -0700

    Fix: RequestHandler contained 'request_' member also

--- src/request_handler.hpp
+++ src/request_handler.hpp
@@ -103,8 +103,6 @@ private:
   void handle_retry_decision(ResponseMessage* response,
                              const RetryPolicy::RetryDecision& decision);
 
-
-  ScopedRefPtr<const Request> request_;
   ScopedRefPtr<ResponseFuture> future_;
   RetryPolicy* retry_policy_;
   int num_retries_;
