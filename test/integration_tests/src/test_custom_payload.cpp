3175dda715aa8fb0d8cb5a849be77a4e9326b590
Michael Fero <michael.fero@datastax.com>
2015-09-19 12:13:34 -0400

    Removing unused includes and limiting test to run with C* 2.2+

--- test/integration_tests/src/test_custom_payload.cpp
+++ test/integration_tests/src/test_custom_payload.cpp
@@ -18,27 +18,12 @@
 #   define BOOST_TEST_MODULE cassandra
 #endif
 
-#include <algorithm>
-
-#if !defined(WIN32) && !defined(_WIN32)
-#include <signal.h>
-#endif
-
 #include "cassandra.h"
-#include "testing.hpp"
 #include "test_utils.hpp"
 #include "cql_ccm_bridge.hpp"
 
 #include <boost/test/unit_test.hpp>
 #include <boost/test/debug.hpp>
-#include <boost/lexical_cast.hpp>
-#include <boost/cstdint.hpp>
-#include <boost/format.hpp>
-#include <boost/asio/io_service.hpp>
-#include <boost/thread/future.hpp>
-#include <boost/bind.hpp>
-#include <boost/chrono.hpp>
-#include <boost/cstdint.hpp>
 
 #include <map>
 #include <set>
@@ -48,58 +33,62 @@ BOOST_AUTO_TEST_SUITE(custom_payload)
 
 BOOST_AUTO_TEST_CASE(simple)
 {
-  test_utils::CassClusterPtr cluster(cass_cluster_new());
-
   const cql::cql_ccm_bridge_configuration_t& conf = cql::get_ccm_bridge_configuration();
-  boost::shared_ptr<cql::cql_ccm_bridge_t> ccm = cql::cql_ccm_bridge_t::create(conf, "test");
+  CassVersion version(conf.cassandara_version());
+  if ((version.major >= 2 && version.minor >= 2) || version.major >= 3) {
+    boost::debug::detect_memory_leaks(false);
+    boost::shared_ptr<cql::cql_ccm_bridge_t> ccm = cql::cql_ccm_bridge_t::create(conf, "test");
 
-  ccm->add_node(1);
-  ccm->start(1, "-Dcassandra.custom_query_handler_class=org.apache.cassandra.cql3.CustomPayloadMirroringQueryHandler");
+    ccm->populate(1);
+    ccm->start(1, "-Dcassandra.custom_query_handler_class=org.apache.cassandra.cql3.CustomPayloadMirroringQueryHandler");
 
-  CassVersion version = ccm->version();
-  BOOST_REQUIRE((version.major >= 2 && version.minor >= 2) || version.major >= 3);
+    test_utils::CassClusterPtr cluster(cass_cluster_new());
+    test_utils::initialize_contact_points(cluster.get(), conf.ip_prefix(), 1, 0);
 
-  test_utils::initialize_contact_points(cluster.get(), conf.ip_prefix(), 1, 0);
+    test_utils::CassSessionPtr session(test_utils::create_session(cluster.get()));
 
-  test_utils::CassSessionPtr session(test_utils::create_session(cluster.get()));
+    test_utils::CassStatementPtr statement(cass_statement_new("SELECT * FROM system.local", 0));
 
-  test_utils::CassStatementPtr statement(cass_statement_new("SELECT * FROM system.local", 0));
+    test_utils::CassCustomPayloadPtr custom_payload(cass_custom_payload_new());
 
-  test_utils::CassCustomPayloadPtr custom_payload(cass_custom_payload_new());
+    typedef std::map<std::string, std::string> PayloadItemMap;
 
-  typedef std::map<std::string, std::string> PayloadItemMap;
+    PayloadItemMap items;
+    items["key1"] = "value1";
+    items["key2"] = "value2";
+    items["key3"] = "value3";
 
-  PayloadItemMap items;
-  items["key1"] = "value1";
-  items["key2"] = "value2";
-  items["key3"] = "value3";
+    for (PayloadItemMap::const_iterator i = items.begin(); i != items.end(); ++i) {
+      cass_custom_payload_set(custom_payload.get(),
+                              i->first.c_str(),
+                              reinterpret_cast<const cass_byte_t*>(i->second.data()), i->second.size());
+    }
 
-  for (PayloadItemMap::const_iterator i = items.begin(); i != items.end(); ++i) {
-    cass_custom_payload_set(custom_payload.get(),
-                            i->first.c_str(),
-                            reinterpret_cast<const cass_byte_t*>(i->second.data()), i->second.size());
-  }
+    cass_statement_set_custom_payload(statement.get(), custom_payload.get());
 
-  cass_statement_set_custom_payload(statement.get(), custom_payload.get());
+    test_utils::CassFuturePtr future(cass_session_execute(session.get(), statement.get()));
 
-  test_utils::CassFuturePtr future(cass_session_execute(session.get(), statement.get()));
+    size_t item_count = cass_future_custom_payload_item_count(future.get());
 
-  size_t item_count = cass_future_custom_payload_item_count(future.get());
+    BOOST_REQUIRE(item_count == items.size());
 
-  BOOST_REQUIRE(item_count == items.size());
+    for (size_t i = 0; i < item_count; ++i) {
+      const char* name;
+      size_t name_length;
+      const cass_byte_t* value;
+      size_t value_size;
+      BOOST_REQUIRE(cass_future_custom_payload_item(future.get(), i,
+                                                    &name, &name_length,
+                                                    &value, &value_size) == CASS_OK);
 
-  for (size_t i = 0; i < item_count; ++i) {
-    const char* name;
-    size_t name_length;
-    const cass_byte_t* value;
-    size_t value_size;
-    BOOST_REQUIRE(cass_future_custom_payload_item(future.get(), i,
-                                                  &name, &name_length,
-                                                  &value, &value_size) == CASS_OK);
-
-    PayloadItemMap::const_iterator it = items.find(std::string(name, name_length));
-    BOOST_REQUIRE(it != items.end());
-    BOOST_CHECK_EQUAL(it->second, std::string(reinterpret_cast<const char*>(value), value_size));
+      PayloadItemMap::const_iterator it = items.find(std::string(name, name_length));
+      BOOST_REQUIRE(it != items.end());
+      BOOST_CHECK_EQUAL(it->second, std::string(reinterpret_cast<const char*>(value), value_size));
+    }
+  } else {
+    boost::unit_test::unit_test_log_t::instance().set_threshold_level(boost::unit_test::log_messages);
+    BOOST_TEST_MESSAGE("Unsupported Test for Cassandra v" << version.to_string() << ": Skipping custom_payload/simple");
+    BOOST_REQUIRE(true);
   }
 }
 
