16561dfdff17f32c914a0f3d57914954e56db9f8
Michael Penick <michael.penick@datastax.com>
2015-09-20 17:54:22 -0700

    Removed unused headers and added date/time unit tests

--- test/unit_tests/src/test_date_time.cpp
+++ test/unit_tests/src/test_date_time.cpp
@@ -21,18 +21,32 @@
 #include "cassandra.h"
 
 #include <boost/test/unit_test.hpp>
-#include <boost/date_time.hpp>
-#include <string.h>
 #include <time.h>
 
 BOOST_AUTO_TEST_SUITE(date_time)
 
 BOOST_AUTO_TEST_CASE(simple)
 {
-  time_t now = time(NULL);
+  time_t now = ::time(NULL);
   cass_uint32_t date = cass_date_from_epoch(now);
   cass_int64_t time = cass_time_from_epoch(now);
   BOOST_CHECK(cass_date_time_to_epoch(date, time) == now);
+}
+
+BOOST_AUTO_TEST_CASE(date)
+{
+  BOOST_CHECK(cass_date_from_epoch(0) ==2147483648u);
+  BOOST_CHECK(cass_date_from_epoch(24 * 3600) ==2147483649u);
+  BOOST_CHECK(cass_date_from_epoch(2 * 24 * 3600) ==2147483650u);
+}
+
+BOOST_AUTO_TEST_CASE(time)
+{
+  time_t now = ::time(NULL);
+  struct tm* tm = gmtime(&now);
+  cass_int64_t actual = cass_time_from_epoch(now);
+  cass_int64_t expected = 1000000000LL * (tm->tm_hour * 3600  + 60 * tm->tm_min + tm->tm_sec);
+  BOOST_CHECK(actual == expected);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
