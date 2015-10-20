9b3fd8a9e5f4f11f3f2f0f7d2c02e8ade35f7bda
Michael Penick <michael.penick@datastax.com>
2015-10-20 08:15:34 -0700

    All column meta constructor should initialize members

--- src/metadata.hpp
+++ src/metadata.hpp
@@ -183,7 +183,7 @@ public:
   ColumnMetadata(const std::string& name)
     : MetadataBase(name)
     , type_(CASS_COLUMN_TYPE_REGULAR)
-    , position_(0)
+    , position_(-1)
     , is_reversed_(false) { }
 
   ColumnMetadata(const std::string& name,
