a00ab383eb2806fb2b4353eaae65a4b14536d00d
Pietro Cerutti <gahr@gahr.ch>
2016-07-01 08:49:09 +0000

    Make sure our in-source include directories come first.
    This avoids including a header from a previously installed version of
    cpp-driver.

--- cmake/modules/CppDriver.cmake
+++ cmake/modules/CppDriver.cmake
@@ -453,10 +453,13 @@ endmacro()
 # Output: CASS_INCLUDES
 #------------------------
 macro(CassAddIncludes)
-  set(INCLUDES ${CASS_INCLUDES} ${CASS_SOURCE_DIR}/include)
-  set(INCLUDES ${INCLUDES} ${CASS_SOURCE_DIR}/src)
-  set(INCLUDES ${INCLUDES} ${CASS_SOURCE_DIR}/src/ssl)
-  set(CASS_INCLUDES ${INCLUDES} ${CASS_SOURCE_DIR}/src/third_party/rapidjson)
+  set(CASS_INCLUDES
+      ${CASS_SOURCE_DIR}/include
+      ${CASS_SOURCE_DIR}/src
+      ${CASS_SOURCE_DIR}/src/ssl
+      ${CASS_SOURCE_DIR}/src/third_party/rapidjson
+      ${CASS_INCLUDES}
+      )
 endmacro()
 
 #------------------------
