e8526b5d328387e84cee47d7a467c980e32bfffe
NeelamAggarwal <neelamsept@gmail.com>
2016-07-22 16:24:01 -0700

    Update collections.c
    Segmentation fault. Corrected by removing the line cass_cluster_set_protocol_version(cluster, 2);

--- examples/collections/collections.c
+++ examples/collections/collections.c
@@ -165,7 +165,7 @@ int main(int argc, char* argv[]) {
 
   const char* items[] = { "apple", "orange", "banana", "mango", NULL };
 
-  cass_cluster_set_protocol_version(cluster, 2);
+  //cass_cluster_set_protocol_version(cluster, 2);
 
   if (argc > 1) {
     hosts = argv[1];
