9511cf97958dcaed9ea2b0c44e893f044347e614
Michael Penick <michael.penick@datastax.com>
2017-03-15 20:28:16 -0400

    Unified encoding of query and execute requests.
    Most of the logic for encoding query and execute requests is the same.
    This unifies most of that logic into the Statement class.

--- cpp-driver/src/query_request.cpp
+++ cpp-driver/src/query_request.cpp
@@ -23,65 +23,37 @@
 
 namespace cass {
 
-int32_t QueryRequest::encode_batch(int version, BufferVec* bufs, RequestCallback* callback) const {
-  int32_t length = 0;
-  const std::string& query(query_);
-
-  // <kind><string><n>[name_1]<value_1>...[name_n]<value_n> ([byte][long string][short][bytes]...[bytes])
-  int buf_size = sizeof(uint8_t) + sizeof(int32_t) + query.size() + sizeof(uint16_t);
-
-  bufs->push_back(Buffer(buf_size));
-  length += buf_size;
-
-  Buffer& buf = bufs->back();
-  size_t pos = buf.encode_byte(0, kind());
-  pos = buf.encode_long_string(pos, query.data(), query.size());
-
-  if (has_names_for_values()) {
-    if (version < 3) {
-      LOG_ERROR("Protocol version %d does not support named values", version);
-      return REQUEST_ERROR_UNSUPPORTED_PROTOCOL;
-    }
-    buf.encode_uint16(pos, value_names_.size());
-    length += copy_buffers_with_names(version, bufs, callback->encoding_cache());
+int QueryRequest::encode(int version, RequestCallback* callback, BufferVec* bufs) const {
+  if (version == 1) {
+    return encode_v1(callback, bufs);
   } else {
-    buf.encode_uint16(pos, elements_count());
-    if (elements_count() > 0) {
-      int32_t result = copy_buffers(version, bufs, callback);
-      if (result < 0) return result;
-      length += result;
+    int32_t length = 0;
+    int32_t result;
+    if (has_names_for_values()) {
+      length += encode_begin(version, value_names_->size(), callback, bufs);
+      result = encode_values_with_names(version, callback, bufs);
+    } else {
+      length += encode_begin(version, elements().size(), callback, bufs);
+      result = encode_values(version, callback, bufs);
     }
+    if (result < 0) return result;
+    length += result;
+    length += encode_end(version, callback, bufs);
+    return length;
   }
-
-  return length;
 }
 
-size_t QueryRequest::get_indices(StringRef name, IndexVec* indices) {
-  set_has_names_for_values(true);
-
-  if (value_names_.get_indices(name, indices) == 0) {
-    if (value_names_.size() > elements_count()) {
-      // No more space left for new named values
-      return 0;
-    }
-    if (name.size() > 0 && name.front() == '"' && name.back() == '"') {
-      name = name.substr(1, name.size() - 2);
-    }
-    indices->push_back(value_names_.add(ValueName(name.to_string())));
-  }
-
-  return indices->size();
-}
-
-int32_t QueryRequest::copy_buffers_with_names(int version,
-                                              BufferVec* bufs,
-                                              EncodingCache* cache) const {
+// Format: [<name_1><value_1>...<name_n><value_n>]
+// where:
+// <name> is a [string]
+// <value> is a [bytes]
+int32_t QueryRequest::encode_values_with_names(int version, RequestCallback* callback, BufferVec* bufs) const {
   int32_t size = 0;
-  for (size_t i = 0; i < value_names_.size(); ++i) {
-    const Buffer& name_buf = value_names_[i].buf;
+  for (size_t i = 0; i < value_names_->size(); ++i) {
+    const Buffer& name_buf = (*value_names_)[i].buf;
     bufs->push_back(name_buf);
 
-    Buffer value_buf(elements()[i].get_buffer_cached(version, cache, false));
+    Buffer value_buf(elements()[i].get_buffer_cached(version, callback->encoding_cache(), false));
     bufs->push_back(value_buf);
 
     size += name_buf.size() + value_buf.size();
@@ -89,109 +61,24 @@ int32_t QueryRequest::copy_buffers_with_names(int version,
   return size;
 }
 
-int QueryRequest::encode(int version, RequestCallback* callback, BufferVec* bufs) const {
-  if (version == 1) {
-    return internal_encode_v1(callback, bufs);
-  } else  {
-    return internal_encode(version, callback, bufs);
-  }
-}
-
-int QueryRequest::internal_encode_v1(RequestCallback* callback, BufferVec* bufs) const {
-  // <query> [long string] + <consistency> [short]
-  size_t length = sizeof(int32_t) + query_.size() + sizeof(uint16_t);
-
-  Buffer buf(length);
-  size_t pos = buf.encode_long_string(0, query_.data(), query_.size());
-  buf.encode_uint16(pos, callback->consistency());
-  bufs->push_back(buf);
-
-  return length;
-}
-
-int QueryRequest::internal_encode(int version, RequestCallback* callback, BufferVec* bufs) const {
-  int length = 0;
-  uint8_t flags = this->flags();
-
-    // <query> [long string] + <consistency> [short] + <flags> [byte]
-  size_t query_buf_size = sizeof(int32_t) + query_.size() +
-                          sizeof(uint16_t) + sizeof(uint8_t);
-  size_t paging_buf_size = 0;
-
-  if (elements_count() > 0) { // <values> = <n><value_1>...<value_n>
-    query_buf_size += sizeof(uint16_t); // <n> [short]
-    flags |= CASS_QUERY_FLAG_VALUES;
+size_t QueryRequest::get_indices(StringRef name, IndexVec* indices) {
+  if (!value_names_) {
+    set_has_names_for_values(true);
+    value_names_.reset(new ValueNameHashTable(elements().size()));
   }
 
-  if (page_size() > 0) {
-    paging_buf_size += sizeof(int32_t); // [int]
-    flags |= CASS_QUERY_FLAG_PAGE_SIZE;
-  }
-
-  if (!paging_state().empty()) {
-    paging_buf_size += sizeof(int32_t) + paging_state().size(); // [bytes]
-    flags |= CASS_QUERY_FLAG_PAGING_STATE;
-  }
-
-  if (serial_consistency() != 0) {
-    paging_buf_size += sizeof(uint16_t); // [short]
-    flags |= CASS_QUERY_FLAG_SERIAL_CONSISTENCY;
-  }
-
-  if (version >= 3 && callback->timestamp() != CASS_INT64_MIN) {
-    paging_buf_size += sizeof(int64_t); // [long]
-    flags |= CASS_QUERY_FLAG_DEFAULT_TIMESTAMP;
-  }
-
-  {
-    bufs->push_back(Buffer(query_buf_size));
-    length += query_buf_size;
-
-    Buffer& buf = bufs->back();
-    size_t pos = buf.encode_long_string(0, query_.data(), query_.size());
-    pos = buf.encode_uint16(pos, callback->consistency());
-    pos = buf.encode_byte(pos, flags);
-
-    if (has_names_for_values()) {
-      if (version < 3) {
-        LOG_ERROR("Protocol version %d does not support named values", version);
-        return REQUEST_ERROR_UNSUPPORTED_PROTOCOL;
-      }
-      buf.encode_uint16(pos, value_names_.size());
-      length += copy_buffers_with_names(version, bufs, callback->encoding_cache());
-    } else if (elements_count() > 0) {
-      buf.encode_uint16(pos, elements_count());
-      int32_t result = copy_buffers(version, bufs, callback);
-      if (result < 0) return result;
-      length += result;
+  if (value_names_->get_indices(name, indices) == 0) {
+    if (value_names_->size() > elements().size()) {
+      // No more space left for new named values
+      return 0;
     }
+    if (name.size() > 0 && name.front() == '"' && name.back() == '"') {
+      name = name.substr(1, name.size() - 2);
+    }
+    indices->push_back(value_names_->add(ValueName(name.to_string())));
   }
 
-  if (paging_buf_size > 0) {
-    bufs->push_back(Buffer(paging_buf_size));
-    length += paging_buf_size;
-
-    Buffer& buf = bufs->back();
-    size_t pos = 0;
-
-    if (page_size() >= 0) {
-      pos = buf.encode_int32(pos, page_size());
-    }
-
-    if (!paging_state().empty()) {
-      pos = buf.encode_bytes(pos, paging_state().data(), paging_state().size());
-    }
-
-    if (serial_consistency() != 0) {
-      pos = buf.encode_uint16(pos, serial_consistency());
-    }
-
-    if (version >= 3 && callback->timestamp() != CASS_INT64_MIN) {
-      pos = buf.encode_int64(pos, callback->timestamp());
-    }
-  }
-
-  return length;
+  return indices->size();
 }
 
 } // namespace cass
--- cpp-driver/src/statement.cpp
+++ cpp-driver/src/statement.cpp
@@ -53,7 +53,7 @@ CassError cass_statement_reset_parameters(CassStatement* statement, size_t count
 
 CassError cass_statement_add_key_index(CassStatement* statement, size_t index) {
   if (statement->kind() != CASS_BATCH_KIND_QUERY) return CASS_ERROR_LIB_BAD_PARAMS;
-  if (index >= statement->elements_count()) return CASS_ERROR_LIB_BAD_PARAMS;
+  if (index >= statement->elements().size()) return CASS_ERROR_LIB_BAD_PARAMS;
   statement->add_key_index(index);
   return CASS_OK;
 }
@@ -250,8 +250,144 @@ CassError cass_statement_bind_custom_by_name_n(CassStatement* statement,
 
 namespace cass {
 
-int32_t Statement::copy_buffers(int version, BufferVec* bufs, RequestCallback* callback) const {
-  int32_t size = 0;
+// Format: <kind><string_or_id><n><value_1>...<value_n>
+// where:
+// <kind> is a [byte]
+// <string_or_id> is a [long string] for <string> and a [short bytes] for <id>
+// <n> is a [short]
+// <value> is a [bytes]
+int32_t Statement::encode_batch(int version, RequestCallback* callback, BufferVec* bufs) const {
+  int32_t length = 0;
+
+  { // <kind> [byte]
+    bufs->push_back(Buffer(sizeof(uint8_t)));
+    Buffer& buf = bufs->back();
+    buf.encode_byte(0, kind());
+    length += sizeof(uint8_t);
+  }
+
+  bufs->push_back(query_or_id_);
+  length += query_or_id_.size();
+
+  { // <n> [short]
+    bufs->push_back(Buffer(sizeof(uint16_t)));
+    Buffer& buf = bufs->back();
+    buf.encode_uint16(0, elements().size());
+    length += sizeof(uint16_t);
+  }
+
+  if (elements().size() > 0) {
+    int32_t result = encode_values(version, callback, bufs);
+    if (result < 0) return result;
+    length += result;
+  }
+
+  return length;
+}
+
+// Format: <string_or_id>[<n><value_1>...<value_n>]<consistency>
+// where:
+// <string_or_id> is a [long string] for <string> and a [short bytes] for <id>
+// <n> is a [short] (only for execute statements)
+// <value> is a [bytes] (only for execute statements)
+// <consistency> is a [short]
+int32_t Statement::encode_v1(RequestCallback* callback, BufferVec* bufs) const {
+  int32_t length = 0;
+  const int version = 1;
+
+  bufs->push_back(query_or_id_);
+  length += query_or_id_.size();
+
+  if (opcode() == CQL_OPCODE_EXECUTE) {
+    { // <n> [short]
+      Buffer buf(sizeof(uint16_t));
+      buf.encode_uint16(0, elements().size());
+      bufs->push_back(buf);
+      length += sizeof(uint16_t);
+    }
+    // <value_1>...<value_n>
+    int32_t result = encode_values(version, callback, bufs);
+    if (result < 0) return result;
+    length += result;
+  }
+
+  { // <consistency> [short]
+    Buffer buf(sizeof(uint16_t));
+    buf.encode_uint16(0, callback->consistency());
+    bufs->push_back(buf);
+    length += sizeof(uint16_t);
+  }
+
+  return length;
+}
+
+// For query statements the format is:
+// <query><consistency><flags><n>
+// where:
+// <query> has the format [long string]
+// <consistency> is a [short]
+// <flags> is a [byte]
+// <n> is a [short]
+//
+// For execute statements the format is:
+// <id><consistency><flags><n>
+// where:
+// <id> has the format [short bytes] (or [string])
+// <consistency> is a [short]
+// <flags> is a [byte]
+// <n> is a [short]
+int32_t Statement::encode_begin(int version, uint16_t element_count,
+                                RequestCallback* callback, BufferVec* bufs) const {
+  int32_t length = 0;
+  size_t query_params_buf_size = 0;
+  uint8_t flags = flags_;
+
+  bufs->push_back(query_or_id_);
+  length += query_or_id_.size();
+
+  query_params_buf_size += sizeof(uint16_t); // <consistency> [short]
+  query_params_buf_size += sizeof(uint8_t); // <flags> [byte]
+
+  if (element_count > 0) {
+    query_params_buf_size += sizeof(uint16_t); // <n> [short]
+    flags |= CASS_QUERY_FLAG_VALUES;
+  }
+
+  if (page_size() > 0) {
+    flags |= CASS_QUERY_FLAG_PAGE_SIZE;
+  }
+
+  if (!paging_state().empty()) {
+    flags |= CASS_QUERY_FLAG_PAGING_STATE;
+  }
+
+  if (serial_consistency() != 0) {
+    flags |= CASS_QUERY_FLAG_SERIAL_CONSISTENCY;
+  }
+
+  if (version >= 3 && callback->timestamp() != CASS_INT64_MIN) {
+    flags |= CASS_QUERY_FLAG_DEFAULT_TIMESTAMP;
+  }
+
+  bufs->push_back(Buffer(query_params_buf_size));
+  length += query_params_buf_size;
+
+  Buffer& buf = bufs->back();
+  size_t pos = buf.encode_uint16(0, callback->consistency());
+  pos = buf.encode_byte(pos, flags);
+
+  if (element_count > 0) {
+    buf.encode_uint16(pos, element_count);
+  }
+
+  return length;
+}
+
+// Format: [<value_1>...<value_n>]
+// where:
+// <value> is a [bytes]
+int32_t Statement::encode_values(int version, RequestCallback* callback, BufferVec* bufs) const {
+  int32_t length = 0;
   for (size_t i = 0; i < elements().size(); ++i) {
     const Element& element = elements()[i];
     if (!element.is_unset()) {
@@ -266,29 +402,83 @@ int32_t Statement::copy_buffers(int version, BufferVec* bufs, RequestCallback* c
         return Request::REQUEST_ERROR_PARAMETER_UNSET;
       }
     }
-    size += bufs->back().size();
+    length += bufs->back().size();
   }
-  return size;
+  return length;
 }
 
-bool Statement::get_routing_key(std::string* routing_key, EncodingCache* cache)  const {
-  if (key_indices_.empty()) return false;
+// Format: [<result_page_size>][<paging_state>][<serial_consistency>][<timestamp>]
+// where:
+// <result_page_size> is a [int]
+// <paging_state> is a [bytes]
+// <serial_consistency> is a [short]
+// <timestamp> is a [long]
+int32_t Statement::encode_end(int version, RequestCallback* callback, BufferVec* bufs) const {
+  int32_t length = 0;
+  size_t paging_buf_size = 0;
 
-  if (key_indices_.size() == 1) {
-      assert(key_indices_.front() < elements_count());
-      const AbstractData::Element& element(elements()[key_indices_.front()]);
-      if (element.is_unset() || element.is_null()) {
-        return false;
-      }
-      Buffer buf(element.get_buffer_cached(CASS_HIGHEST_SUPPORTED_PROTOCOL_VERSION, cache, true));
-      routing_key->assign(buf.data() + sizeof(int32_t),
-                          buf.size() - sizeof(int32_t));
+  if (page_size() > 0) {
+    paging_buf_size += sizeof(int32_t); // [int]
+  }
+
+  if (!paging_state().empty()) {
+    paging_buf_size += sizeof(int32_t) + paging_state().size(); // [bytes]
+  }
+
+  if (serial_consistency() != 0) {
+    paging_buf_size += sizeof(uint16_t); // [short]
+  }
+
+  if (version >= 3 && callback->timestamp() != CASS_INT64_MIN) {
+    paging_buf_size += sizeof(int64_t); // [long]
+  }
+
+  if (paging_buf_size > 0) {
+    bufs->push_back(Buffer(paging_buf_size));
+    length += paging_buf_size;
+
+    Buffer& buf = bufs->back();
+    size_t pos = 0;
+
+    if (page_size() >= 0) {
+      pos = buf.encode_int32(pos, page_size());
+    }
+
+    if (!paging_state().empty()) {
+      pos = buf.encode_bytes(pos, paging_state().data(), paging_state().size());
+    }
+
+    if (serial_consistency() != 0) {
+      pos = buf.encode_uint16(pos, serial_consistency());
+    }
+
+    if (version >= 3 && callback->timestamp() != CASS_INT64_MIN) {
+      pos = buf.encode_int64(pos, callback->timestamp());
+    }
+  }
+
+  return length;
+}
+
+bool Statement::calculate_routing_key(const std::vector<size_t>& key_indices,
+                                      std::string* routing_key, EncodingCache* cache) const {
+  if (key_indices.empty()) return false;
+
+  if (key_indices.size() == 1) {
+    assert(key_indices.front() < elements().size());
+    const AbstractData::Element& element(elements()[key_indices.front()]);
+    if (element.is_unset() || element.is_null()) {
+      return false;
+    }
+    Buffer buf(element.get_buffer_cached(CASS_HIGHEST_SUPPORTED_PROTOCOL_VERSION, cache, true));
+    routing_key->assign(buf.data() + sizeof(int32_t),
+                        buf.size() - sizeof(int32_t));
   } else {
     size_t length = 0;
 
-    for (std::vector<size_t>::const_iterator i = key_indices_.begin();
-         i != key_indices_.end(); ++i) {
-      assert(*i < elements_count());
+    for (std::vector<size_t>::const_iterator i = key_indices.begin();
+         i != key_indices.end(); ++i) {
+      assert(*i < elements().size());
       const AbstractData::Element& element(elements()[*i]);
       if (element.is_unset() || element.is_null()) {
         return false;
@@ -300,8 +490,8 @@ bool Statement::get_routing_key(std::string* routing_key, EncodingCache* cache) 
     routing_key->clear();
     routing_key->reserve(length);
 
-    for (std::vector<size_t>::const_iterator i = key_indices_.begin();
-         i != key_indices_.end(); ++i) {
+    for (std::vector<size_t>::const_iterator i = key_indices.begin();
+         i != key_indices.end(); ++i) {
       const AbstractData::Element& element(elements()[*i]);
       Buffer buf(element.get_buffer_cached(CASS_HIGHEST_SUPPORTED_PROTOCOL_VERSION, cache, true));
       size_t size = buf.size() - sizeof(int32_t);
