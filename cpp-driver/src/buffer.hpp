8f53692a470505e7644c1f1f0f8b55584da2a701
Michael Fero <michael.fero@datastax.com>
2017-10-06 20:54:40 +0000

    CPP-438: Fixing compiler issues with GCC 7
    * Uninitialized buffer in union

--- cpp-driver/src/buffer.hpp
+++ cpp-driver/src/buffer.hpp
@@ -211,9 +211,12 @@ private:
     size_ = buf.size_;
   }
 
-  union {
+  union Data {
     char fixed[FIXED_BUFFER_SIZE];
     RefBuffer* buffer;
+
+    Data()
+      : buffer(NULL) { }
   } data_;
 
   size_t size_;
