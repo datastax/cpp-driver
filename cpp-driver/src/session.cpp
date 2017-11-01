38a7c4dd176a45de43cebe50f696bf04d823937c
Michael Penick <michael.penick@datastax.com>
2017-11-01 09:41:51 -0400

    CPP-538 - Set available connections to total connections

--- cpp-driver/src/session.cpp
+++ cpp-driver/src/session.cpp
@@ -134,6 +134,7 @@ void  cass_session_get_metrics(const CassSession* session,
   metrics->requests.mean_rate = internal_metrics->request_rates.mean_rate();
 
   metrics->stats.total_connections = internal_metrics->total_connections.sum();
+  metrics->stats.available_connections = metrics->stats.total_connections; // Deprecated
   metrics->stats.exceeded_write_bytes_water_mark = 0; // Deprecated
   metrics->stats.exceeded_pending_requests_water_mark = 0; // Deprecated
 
