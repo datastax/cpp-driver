9e03d9284ef1f4f25d2ec5551dd36c139a4ba7dd
Michael Penick <michael.penick@datastax.com>
2018-06-22 10:51:48 -0400

    Fix include guard

--- cpp-driver/src/loop_watcher.hpp
+++ cpp-driver/src/loop_watcher.hpp
@@ -14,8 +14,8 @@
   limitations under the License.
 */
 
-#ifndef __CASS_PREPARE_HPP_INCLUDED__
-#define __CASS_PREPARE_HPP_INCLUDED__
+#ifndef __CASS_LOOP_WATCHER_HPP_INCLUDED__
+#define __CASS_LOOP_WATCHER_HPP_INCLUDED__
 
 #include "callback.hpp"
 #include "macros.hpp"
