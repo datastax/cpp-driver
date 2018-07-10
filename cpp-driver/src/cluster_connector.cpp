308992bddfd81c463dfe2c70a0e66f6c8797b8a9
Michael Penick <michael.penick@datastax.com>
2018-07-10 12:42:25 -0400

    Fix for the control connection not being closed
    The control connection wasn't being properly closed when there were no
    hosts available because of an invalid DC.

--- cpp-driver/src/cluster_connector.cpp
+++ cpp-driver/src/cluster_connector.cpp
@@ -134,6 +134,7 @@ void ClusterConnector::internal_cancel() {
   error_code_ = CLUSTER_CANCELED;
   if (resolver_) resolver_->cancel();
   if (connector_) connector_->cancel();
+  if (cluster_) cluster_->close();
 }
 
 void ClusterConnector::finish() {
@@ -196,18 +197,16 @@ void ClusterConnector::on_connect(ControlConnector* connector) {
   }
 
   if (connector->is_ok()) {
-    ControlConnection::Ptr connection = connector->release_connection();
     const HostMap& hosts(connector->hosts());
     LoadBalancingPolicy::Vec policies;
 
-    HostMap::const_iterator host_it = hosts.find(connection->address());
+    HostMap::const_iterator host_it = hosts.find(connector->address());
     if (host_it == hosts.end()) {
       // This error is unlikely to happen and it likely means that the local
       // metadata is corrupt or missing and the control connection process
       // would've probably failed before this happens.
       LOG_ERROR("Current control connection host %s not found in hosts metadata",
-                connection->address_string().c_str());
-
+                connector_->address().to_string().c_str());
       ++contact_points_resolved_it_; // Move to the next contact point
       internal_connect();
       return;
@@ -247,7 +246,7 @@ void ClusterConnector::on_connect(ControlConnector* connector) {
       return;
     }
 
-    cluster_.reset(Memory::allocate<Cluster>(connection,
+    cluster_.reset(Memory::allocate<Cluster>(connector->release_connection(),
                                              listener_,
                                              event_loop_,
                                              connected_host,
