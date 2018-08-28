cfc20663625bffaaddbaae98bbace80e35447495
Michael Penick <michael.penick@datastax.com>
2018-08-28 09:58:03 -0400

    Revert "Merge pull request #165 from riptano/CPP-666"
    This reverts commit 43dc1703aa2674c7ca901a90e75b5f427b8948d7, reversing
    changes made to 5a85dbede29571f0150217c2ce3f99d6f2976c56.

--- cpp-driver/src/metadata.hpp
+++ cpp-driver/src/metadata.hpp
@@ -496,7 +496,8 @@ public:
   Iterator* iterator_views() const { return Memory::allocate<ViewIteratorVec>(views_); }
   const ViewMetadata* get_view(const String& name) const;
   virtual void add_column(const VersionNumber& server_version, const ColumnMetadata::Ptr& column);
-  void set_views(const ViewMetadata::Vec& views);
+  void add_view(const ViewMetadata::Ptr& view);
+  void sort_views();
 
   Iterator* iterator_indexes() const { return Memory::allocate<IndexIterator>(indexes_); }
   const IndexMetadata* get_index(const String& name) const;
@@ -590,6 +591,10 @@ public:
 
   StringRef strategy_class() const { return strategy_class_; }
   const Value* strategy_options() const { return &strategy_options_; }
+
+private:
+  void internal_add_table(const TableMetadata::Ptr& table,
+                          const ViewMetadata::Vec& views);
 
 private:
   const bool is_virtual_;
