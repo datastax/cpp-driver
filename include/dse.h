38e4ad803fb1f6cc6343fe309206b9ed7254b616
Michael Penick <michael.penick@datastax.com>
2018-09-13 09:30:45 -0400

    Update version numbers for 1.6.0/2.10.0 GA

--- include/dse.h
+++ include/dse.h
@@ -51,7 +51,7 @@
 #define DSE_VERSION_MAJOR 1
 #define DSE_VERSION_MINOR 6
 #define DSE_VERSION_PATCH 0
-#define DSE_VERSION_SUFFIX "beta1"
+#define DSE_VERSION_SUFFIX ""
 
 #ifdef __cplusplus
 extern "C" {
