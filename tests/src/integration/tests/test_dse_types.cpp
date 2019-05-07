e87cafb98a10dedb102bab6babcd8ac68219c78d
Michael Fero <michael.fero@datastax.com>
2019-05-07 13:45:02 -0400

    Manually fixing code format before re-running clang-format

--- tests/src/integration/tests/test_dse_types.cpp
+++ tests/src/integration/tests/test_dse_types.cpp
@@ -438,8 +438,7 @@ const std::vector<dse::LineString>
  * Values for polygon tests
  */
 const dse::Polygon GEOMETRY_POLYGON[] = { dse::Polygon("(1.0 3.0, 3.0 1.0, 3.0 6.0, 1.0 3.0)"),
-                                          dse::Polygon("(0.0 10.0, 10.0 0.0, 10.0 10.0, 0.0 10.0), \
-                (6.0 7.0, 3.0 9.0, 9.0 9.0, 6.0 7.0)"),
+                                          dse::Polygon("(0.0 10.0, 10.0 0.0, 10.0 10.0, 0.0 10.0), (6.0 7.0, 3.0 9.0, 9.0 9.0, 6.0 7.0)"),
                                           dse::Polygon("POLYGON EMPTY") };
 template <>
 const std::vector<dse::Polygon>
