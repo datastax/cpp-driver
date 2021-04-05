a10baf1c5c6c7bedd34cfee666035aa227babe9e
Alex Revetchi <alex.revetchi@gmail.com>
2021-04-05 17:24:11 +0100

    - apply make format

--- src/decoder.hpp
+++ src/decoder.hpp
@@ -558,7 +558,7 @@ public:
   bool decode_write_type(CassWriteType& output);
   bool decode_warnings(WarningVec& output);
 
-  Value  decode_value(const DataType::ConstPtr& data_type);
+  Value decode_value(const DataType::ConstPtr& data_type);
 
 protected:
   // Testing only
--- src/row.cpp
+++ src/row.cpp
@@ -49,8 +49,7 @@ const CassValue* cass_row_get_column_by_name_n(const CassRow* row, const char* n
 
 namespace datastax { namespace internal { namespace core {
 
-bool decode_row(Decoder& decoder, const ResultResponse* result,
-                OutputValueVec& output) {
+bool decode_row(Decoder& decoder, const ResultResponse* result, OutputValueVec& output) {
   output.clear();
   const int column_count = result->column_count();
   if (column_count > 0) {
@@ -61,7 +60,8 @@ bool decode_row(Decoder& decoder, const ResultResponse* result,
       Value value = decoder.decode_value(def.data_type);
       if (value.data_type()) {
         output.push_back(value);
-      } else return false;
+      } else
+        return false;
     }
   }
   return true;
