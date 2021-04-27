edf6be1680a8950d25af0c6f165dd90edcb71ddf
Alex Revetchi <alex.revetchi@gmail.com>
2021-04-27 22:17:09 +0100

    - apply formatting

--- tests/src/unit/tests/test_dse_polygon.cpp
+++ tests/src/unit/tests/test_dse_polygon.cpp
@@ -40,8 +40,8 @@ public:
     // normally DataType lifecycle is dictated by the response object
     static const DataType::ConstPtr data_type(new CustomType(DSE_POLYGON_TYPE));
     char* data = const_cast<char*>(reinterpret_cast<const char*>(polygon->bytes().data()));
-    value = Value(data_type,
-                  Decoder(data, polygon->bytes().size(), 0)); // Protocol version not used
+    value =
+        Value(data_type, Decoder(data, polygon->bytes().size(), 0)); // Protocol version not used
     return CassValue::to(&value);
   }
 
