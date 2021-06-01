a92202a1dcbcc2d8ea4f94de6480550894bc0db1
Alex Revetchi <alex.revetchi@gmail.com>
2021-06-01 14:20:31 +0100

    - address review comment, handle tuple & udt cases

--- src/value.cpp
+++ src/value.cpp
@@ -214,6 +214,12 @@ bool Value::update(const Decoder& decoder) {
   if (!decoder_.is_null()) {
     if (data_type_->is_collection()) {
       return decoder_.decode_int32(count_);
+    } else if (data_type->is_tuple()) {
+      const CompositeType& composite_type = static_cast<const CompositeType&>(*data_type);
+      count_ = composite_type.types().size();
+    } else if (data_type->is_user_type()) {
+      const UserType& user_type = static_cast<const UserType&>(*data_type);
+      count_ = user_type.fields().size();
     }
   } else {
     count_ = 0;
