7319176df2371922e244aee564b3228ff04ddf2b
Bret McGuire <bret.mcguire@datastax.com>
2022-03-28 17:04:12 -0500

    Initial fix for deprecated warnings

--- tests/src/integration/objects/cluster.hpp
+++ tests/src/integration/objects/cluster.hpp
@@ -216,11 +216,18 @@ public:
    *               otherwise (default: true)
    * @return Cluster object
    */
+  #if defined(__GNUC__) || defined(__INTEL_COMPILER)
+  #  pragma GCC diagnostic push
+  #  pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+  #endif
   Cluster& with_hostname_resolution(bool enable = true) {
     EXPECT_EQ(CASS_OK, cass_cluster_set_use_hostname_resolution(
                            get(), (enable == true ? cass_true : cass_false)));
     return *this;
   }
+  #if defined(__GNUC__) || defined(__INTEL_COMPILER)
+  #  pragma GCC diagnostic pop
+  #endif
 
   /**
    * Sets the number of I/O threads. This is the number of threads that will
